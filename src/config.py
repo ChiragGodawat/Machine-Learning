9587471428565bf41f51b408aab2936951f9ac47
Chirag Godawat <c.godawat@gmail.com>
2021-12-19 22:44:58 +0530

    Updating csv to get file size in limits for git push

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
-MNIST_TRAIN = "Input/MNIST/mnist_train.csv"
-MNIST_TRAIN_FOLDS = "Input/MNIST/mnist_train_folds.csv"
+MNIST_TRAIN = "Input/MNIST/mnist_train_data.csv"
+MNIST_TRAIN_FOLDS = "Input/MNIST/mnist_train_folds_data.csv"
 MNIST_MODEL_OUTPUT = "Models/MNIST/"
