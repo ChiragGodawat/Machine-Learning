6c5b21f2b7b2a3789b2f06eff17f4ace685104aa
Chirag Godawat <c.godawat@gmail.com>
2022-01-01 12:01:20 +0530

    Refactoring

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+# Set paths in a way that the program is always run from root i.e Machine Learning folder
 MNIST_TRAIN = "Input/MNIST/mnist_train.csv"
 MNIST_TRAIN_FOLDS = "Input/MNIST/mnist_train_folds.csv"
 MNIST_MODEL_OUTPUT = "Models/MNIST/"
