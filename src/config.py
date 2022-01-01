c1e8412cacaf65e045a13ee4f365da9bc3447823
Chirag Godawat <c.godawat@gmail.com>
2022-01-01 11:51:03 +0530

    Code for creating kfold for cat in the dat

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 MNIST_TRAIN = "Input/MNIST/mnist_train.csv"
 MNIST_TRAIN_FOLDS = "Input/MNIST/mnist_train_folds.csv"
 MNIST_MODEL_OUTPUT = "Models/MNIST/"
+CAT_DAT_TRAIN = "Input/Cat_in_Dat/train.csv"
+CAT_DAT_TRAIN_FOLD = "Input/Cat_in_Dat/train_folds.csv"
--- src/create_folds.py
+++ src/create_folds.py
@@ -17,3 +17,22 @@ def create_k_fold_mnist_csv():
         df.loc[_v, "kfold"] = f
 
     df.to_csv(config.MNIST_TRAIN_FOLDS, index=False)
+
+
+def create_k_fold_cat_dat_csv():
+
+    print("Creating K Fold CSV for Cat in Dat dataset")
+
+    # Download original dataset from https://www.kaggle.com/c/cat-in-the-dat-ii/data and place it in Input folder
+    df = pd.read_csv(config.CAT_DAT_TRAIN)
+    
+    df["kfold"] = -1
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    y = df.target.values
+
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    for f, (t_, v_) in enumerate(kf.split(X=df,y=y)):
+        df.loc[v_, "kfold"] = f
+
+    df.to_csv(config.CAT_DAT_TRAIN_FOLD, index=False)
