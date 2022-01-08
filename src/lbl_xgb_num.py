cfbfca0a66aca8e4344ef458e2304109c9e6405e
Chirag Godawat <c.godawat@gmail.com>
2022-01-08 18:53:30 +0530

    Updating lbl xgb numerical

--- src/lbl_xgb_num.py
+++ src/lbl_xgb_num.py
@@ -9,34 +9,6 @@ from sklearn import metrics
 import config
 
 
-def run_cat_in_dat(fold):
-    df = pd.read_csv(config.CAT_DAT_TRAIN_FOLD)
-    features = [f for f in df.columns if f not in ('id', 'target', 'kfold')]
-    for col in features:
-        df[col] = df[col].astype(str).fillna("NONE")
-
-    for col in features:
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(df[col])
-        df[col] = lbl.transform(df[col])
-
-    df_train = df[df['kfold'] != fold].reset_index(drop=True)
-    df_valid = df[df['kfold'] == fold].reset_index(drop=True)
-
-    x_train = df_train[features].values
-    x_valid = df_valid[features].values
-
-    model = xgb.XGBClassifier(max_depth=10, n_estimators=200, n_jobs=-1)
-
-    model.fit(x_train, df_train.target.values)
-
-    preds = model.predict_proba(x_valid)[:, 1]
-
-    roc_auc = metrics.roc_auc_score(df_valid.target.values, preds)
-
-    print(f"Fold: {fold}   AUC: {roc_auc}")
-
-
 def run_census_adult(fold):
     df = pd.read_csv(config.CENSUS_TRAIN_FOLD)
 
